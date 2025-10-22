b44a09904f56e7a14da6bbe60fa2e42a83fdc73e
Nitish Kumar <kumarnitishbxr07@gmail.com>
2025-10-22 16:08:09 +0530

    Update axios client for Render deployment

--- Client/src/API/axiosClient.js
+++ Client/src/API/axiosClient.js
@@ -1,16 +1,30 @@
-import axios from "axios"
+// import axios from "axios"
 
-const axiosClient =  axios.create({
+// const axiosClient =  axios.create({
 
-    baseURL: 'http://localhost:3000',
+//     baseURL: 'http://localhost:3000',
 
-    withCredentials: true,
-    headers: {
-      'Content-Type': 'application/json'
-    }
+//     withCredentials: true,
+//     headers: {
+//       'Content-Type': 'application/json'
+//     }
 
+// });
+
+
+// export default axiosClient;
+
+
+import axios from "axios";
+
+// Use environment variable for backend URL
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
-
 
 export default axiosClient;
 
